56af648af6a3ae1fe990d5820c88bc7abdb89684
ropreller <r.opreller@gmail.com>
2022-05-16 00:07:02 -0400

    Envío tarea componente.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Productos :titles="tableRender.titles" :products="tableRender.products" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Productos from './components/Productos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Productos
+  },
+  data (){
+    return {
+            tableRender : {
+            titles: ['ID','Precio','Foto','Nombre','Descripcion','Stock','Añadir'],
+            products: [
+                {
+                    id: 1,
+                    name: 'Churu clásico unidad',
+                    price: 1500,
+                    desc: 'Clásico churu sabor atún.',
+                    img: './assets/img/churu.jpg',
+                    stock: 2
+
+                },
+                {
+                    id: 2,
+                    name: 'Bravery 7kg.',
+                    price: 45000,
+                    desc: 'Alimento Bravery sabor salmón.',
+                    img: './assets/img/bravery.jpg',
+                    stock: 0
+                },
+                {
+                    id: 3,
+                    name: 'Matisse 7kg.',
+                    price: 43200,
+                    desc: 'Alimento Mattise sabor salmón.',
+                    img: './assets/img/mattise.jpg',
+                    stock: 3
+                },
+                {
+                    id: 4,
+                    name: 'Leonardo lata.',
+                    price: 3450,
+                    desc: 'Alimento humedo marca exclusiva.',
+                    img: './assets/img/leonardo.jpg',
+                    stock: 7
+                },
+                {
+                    id: 5,
+                    name: 'Leonardo kitten.',
+                    price: 46200,
+                    desc: 'Leonardo kitten sabor inigualable.',
+                    img: './assets/img/Leonardo_2.jpg',
+                    stock: 0
+                },
+                {
+                    id: 6,
+                    name: 'Proplan urinary.',
+                    price: 33600,
+                    desc: 'Para el cuidado de sus riñones.',
+                    img: './assets/img/proplan_urinary.jpg',
+                    stock: 1
+                }
+            ]
+        },
+    }
   }
 }
 </script>
---
+++ src/components/Productos.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+       <div>
+    <table class="table">
+            <thead>
+                <tr>
+                    <th v-for="(item, i) in titles" :key="i" scope="col">{{ item }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, i) in products" :key="i">
+                   <td>{{ item.id }}</td>
+                   <td>{{ item.price }}</td>
+                   <td><img :src="item.img" class="imgTabla" /></td>
+                   <td>{{ item.name }}</td>
+                   <td>{{ item.desc }}</td>
+                   <td>{{ item.stock }}</td>
+                   <td>
+                   <button v-if="item.stock>0" type="button" class="btn btn-success">Añadir</button>
+                   <p v-else class="noDisponible">No disponible</p>
+                   </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titles: {
+            type: Array,
+            required: true
+        },
+        products: {
+            type: Array,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+/*
+* Autor: ropreller
+* Año: 2022
+* Desc: CSS customizado para ajustes.
+*/
+
+.imgTabla {
+    max-width: 100px;
+}
+
+.noDisponible {
+    color:red
+}
+
+.tdNoDisponible {
+    background-color: rgb(226, 226, 226);
+}
+
+</style>
